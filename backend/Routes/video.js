4a49046cd89eaf2db9e1698d050e43852dda516c
Steven <sc201789@ucf.edu>
2025-02-27 10:23:08 -0500

    removing storage on google cloud

--- backend/Routes/video.js
+++ backend/Routes/video.js
@@ -8,7 +8,6 @@ const { processVideo } = require('../Routes_help/videoProcessor'); // Import the
 const {pick_p} = require('../Routes_help/pickPlayer');
 const {send_completion_email} = require('../Routes_help/v_email'); //send verification email
 const {convertToH264} = require("../Routes_help/convert_video"); //convert video to valid format for website display
-const {uploadGCS, generate_signed_url} = require("../Routes_help/storage"); //storage functions for videos
 const Video = require('../Schemas/VideoSchema'); //video model
 const User = require('../Schemas/UserSchema'); //user model
 // Configure multer to store uploaded files in the 'inputs' folder temporarily
@@ -134,130 +133,6 @@ router.post('/send_completion', async(req, res)=> {
     }
 
     
-});
-//CHECKED for 200, 400, add functionality to check only for user list, adjust to store data in non-cloud database
-router.post('/save_video', verifyToken, async(req, res) =>{
-    try{
-        const {userId, filepath, video_name} = req.body; //userId, filepath, video name should be entered
-        
-        console.log(userId);
-        console.log(filepath); //./outputs/54b1d3ffcd2f019ccf07a9cf50e48773CONV.mp4
-        console.log(video_name)
-
-        // 'path' in bucket specified with username and video_name
-        const GCSPath=`${userId}/${video_name}`; 
-        
-        //check if video is already saved in system REVISE
-        const Video_name = await Video.findOne({ //check if video already saved in database
-            where: {userId:userId,
-                    video_name:video_name}
-        });
-        if (Video_name){ 
-            return res.status(400).json({error: 'video already saved'});
-        }
-
-        //grab user by id
-        //const user = await User.findByPk(userId);
-
-        let url;
-
-        if (useCloud){
-            //upload to google bucket
-            await uploadGCS(filepath, GCSPath);
-
-            //create url to store in database, access for viewing/download is available for a weekish
-            url = await generate_signed_url(GCSPath);
-        }
-
-        else{
-            url = GCSPath;
-        }
-
-        console.log(`url: ${url}`);
-        //create video associated with user and store accessible username in database
-        await Video.create({
-            userId: userId,
-            Url: url,
-            video_name: video_name
-        });
-
-        res.status(200).json({message: 'successfully saved video'});
-    }catch(error){
-        console.error(error);
-        res.status(500).json({error: 'video not saved'});
-    }
-});
-
-//CHECKED for 200, 401
-router.post('/delete_video', verifyToken, async(req, res)=>{ //check if token present
-    try{
-
-        const {userId, video_name}=req.body; //user id but video name most important
-
-        //check if user exists, possibly remove
-        //const user = await User.findByPk(userId); 
-        //if (!user){
-            //return res.status(400).json({message: 'user not found'});
-        //}
-
-        //check if video exists matching user
-        const video = await Video.findOne({
-            where: {
-                userId:userId,
-                ...(video_name && {video_name: {[Op.like]: `%${video_name}%`}})
-            }
-        });
-        if (!video){
-            return res.status(401).json({message: 'video file not found'});
-        }
-
-        //delete video
-        await video.destroy();
-
-        //success message
-        res.status(200).json({message: 'video file successfully destroyed'});
-
-    }catch(error){
-        console.error(error);
-        res.status(500).json({error: 'Failed to destroy video'});
-    }
-});
-
-//CHECKED for 200, 400
-router.post('/search_video', verifyToken, async(req, res)=>{
-    try{
-        const {userId, video_name}=req.body; //user id but video name most important
-        
-        //check if video exists matching user
-        const video = await Video.findOne({
-            where:{
-                userId:userId,
-                ...(video_name && {video_name: {[Op.like]: `%${video_name}%`}})
-            }
-        });        
-        if (!video) {
-            return res.status(400).json({ message: 'No video by that name found for this user' });
-        }
-        
-        //return video if found, check for correctness
-        res.status(200).json({message: "video found", video});
-        
-    }catch(error){
-        console.error(error);
-        res.status(500).json({error:'video search unsuccessful'});
-    }
-});
-
-router.get('/saved-videos/:userId', verifyToken, async(req, res)=>{
-    try{
-        const {userId} = req.params;
-
-        const saved_videos = await Video.find({where: {userId:userId}});
-
-        res.status(200).json(saved_videos);
-    }catch(error){
-        res.status(500).send(error);
-    }
 });
 
 router.get('/download_video', verifyToken, async(req, res)=>{
